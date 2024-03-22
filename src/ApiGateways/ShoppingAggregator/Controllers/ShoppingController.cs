78369c4cb1f6468c07e1f9d042e5841a0f0634fe
Oleg Shilov <oshilov@lanit.ru>
2024-03-22 23:38:36 +0300

    Shopping.Aggregator is ready now

---
+++ src/ApiGateways/ShoppingAggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace Shopping.Aggregator.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ShoppingController : ControllerBase
+{
+    private readonly ICatalogService _catalogService;
+    private readonly IBasketService _basketService;
+    private readonly IOrderService _orderService;
+
+    public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+    {
+        _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+        _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+    }
+
+    [HttpGet("{userName}", Name = "GetShopping")]
+    [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+    {
+        var basket = await _basketService.GetBasket(userName);
+        foreach (var basketItem in basket.Items) 
+        {
+            var product = await _catalogService.GetCatalog(basketItem.ProductId);
+
+            basketItem.ProductName = product.Name;
+            basketItem.Category = product.Category;
+            basketItem.Summary = product.Summary;
+            basketItem.Description = product.Description;
+            basketItem.ImageFile = product.ImageFile;
+        }
+        var orders = await _orderService.GetOrdersByUserName(userName);
+
+        var shoppingModel = new ShoppingModel
+        {
+            UserName = userName,
+            BasketWithProducts = basket,
+            Orders = orders
+        };
+
+        return Ok(shoppingModel);
+    }
+}
---
+++ src/ApiGateways/ShoppingAggregator/Program.cs
@@ -0,0 +1,33 @@
+using Shopping.Aggregator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Add http client services at ConfigureServices(IServiceCollection services)
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(client =>
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]));
+builder.Services.AddHttpClient<IBasketService, BasketService>(client =>
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
+builder.Services.AddHttpClient<IOrderService, OrderService>(client =>
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
